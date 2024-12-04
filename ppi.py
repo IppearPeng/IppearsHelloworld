0b7abf2de444de85d46db393249eff0bce335ea2
IppearPeng <pongyipai@163.com>
2024-12-04 15:32:16 +0800

    Add files via upload

---
+++ ppi.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 16:17:28 2024
+
+@author: IppearPeng
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ppi(x,y,inch):
+    return (x**2+y**2)**0.5/inch
+
+size=np.linspace(1,110,num=219)
+qhd=ppi(2560,1440,size)
+
+plt.plot(size,qhd)
